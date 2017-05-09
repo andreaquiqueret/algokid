a5b9768c64dbaea87af6d4a726675c3b9f12fd5d
Aurélien Rausch <aurel@aurel-r.fr>
2017-05-09 16:58:47 +0200

    ajout de methodes au modele

--- Fonction.java
+++ Fonction.java
@@ -1,10 +1,35 @@
 public class Function extends Instruction {
 	private ArrayList<Instruction> instructions;
-	private int id;
+	private String nom;
 
 	public Fonction()
 	{
 		super();
+		this.instructions = new ArrayList<Instruction>();
+		this.nom = "Principale";
+	}
+
+	public Fonction(ArrayList<Instruction> i, String nom)
+	{
+		this.instructions = new ArrayList<Instruction>(i);
+		this.nom = new String(nom);
+	}
+
+	public setInstructions(ArrayList<Instruction> i)
+	{
+		this.instruction = new ArrayList<Instruction>(i);
+	}
+
+
+
+	public setNom(String nom)
+	{
+		this.nom = new String(nom);
+	}
+
+	public String getNom()
+	{
+		return this.nom;
 	}
 
 	public void executer()
@@ -12,16 +37,9 @@ public class Function extends Instruction {
 
 	}
 	
-	public void add_fonction(int id)
+	public void add_fonction(Fonction f, int index)
 	{
-	}
 
-	public void add_action(int num_action)
-	{
-	}
-	
-	public void add_fonction(int id, int index)
-	{
 	}
 
 	public void add_action(int num_action, int index)
