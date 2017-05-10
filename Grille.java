3aa785e9a13289afb2e1b393d6ea3edb41c8bdf4
Aurélien Rausch <aurel@aurel-r.fr>
2017-05-10 10:44:07 +0200

    ajout des constructeurs

--- Grille.java
+++ Grille.java
@@ -1,6 +1,16 @@
 public class Grille  {
 
-	private int cellules[][];
+	private final int width = 10;
+	private final int height = 10;
+	private static int cellules[][];
 	private Robot r;
-	
+
+	public Grille() {
+		this.r = new Robot();
+		this.cellules = new int[height][width];
+		for (int i = 0; i < height; i++)
+			for (int j = 0; j < width; j++)
+				this.cellules[i][j] = 0;
+	}
+
 	}
--- Robot.java
+++ Robot.java
@@ -1,7 +1,13 @@
-public class Robot {
+public class Robot implements Orientation {
 
 	private int posX;
 	private int posY;
 	private int orientation;
-	
+
+	public Robot() {
+		this.posX = 0;
+		this.posy = 0;
+		this.orientation = HAUT;
 	}
+
+}
