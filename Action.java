6c5c064e4ec2b57d30e876297cef63bf466e6611
Aurélien Rausch <aurel@aurel-r.fr>
2017-05-10 11:27:58 +0200

    Ajout de la methode executer()

--- Action.java
+++ Action.java
@@ -14,6 +14,48 @@ public class Action implements Orientation extends Instruction {
 
 	public void executer()
 	{
+		Robot r = Grille.getRobot();
+		int o = r.getOrientation();
+		int x = r.getPosX();
+		int y = r.getPosY();
 
+		if (action_num == GAUCHE || action_num == DROITE)
+			r.setOrientation(action_num);
+		
+		if (action_num == HAUT) { // avancer
+			switch (o) {
+			case HAUT: 
+				r.setPosY(y + 1);
+				break;
+			case BAS:
+				r.setPosY(y - 1);
+				break;
+			case GAUCHE:
+				r.setPosX(x - 1);
+				break;
+			case DROITE:
+				r.setPosX(x + 1);
+				break;
+			}
+		}
+
+		if (action_num == BAS) { // reculer
+			switch (o) {
+			case HAUT: 
+				r.setPosY(y - 1);
+				break;
+			case BAS:
+				r.setPosY(y + 1);
+				break;
+			case GAUCHE:
+				r.setPosX(x + 1);
+				break;
+			case DROITE:
+				r.setPosX(x - 1);
+				break;
+			}
+		}
+
+		Grille.updateGrille();
 	}
 }
