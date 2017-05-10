9cda1589c2584e621c6ef8f2a934ab2a62db7621
Aurélien Rausch <aurel@aurel-r.fr>
2017-05-10 11:29:27 +0200

    definition de la grille

--- Grille.java
+++ Grille.java
@@ -1,16 +1,33 @@
 public class Grille  {
 
-	private final int width = 10;
-	private final int height = 10;
-	private static int cellules[][];
+	public static final int WIDTH = 10;
+	public static final int HEIGHT = 10;
+	public static int cellules[][];
 	private Robot r;
 
 	public Grille() {
 		this.r = new Robot();
-		this.cellules = new int[height][width];
-		for (int i = 0; i < height; i++)
-			for (int j = 0; j < width; j++)
+		this.cellules = new int[HEIGHT][WIDTH];
+		for (int i = 0; i < HEIGHT; i++)
+			for (int j = 0; j < WIDTH; j++)
 				this.cellules[i][j] = 0;
+		this.cellules[HEIGHT - 1][0] = 1;
 	}
 
+	public Robot getRobot() {
+		return this.r;
 	}
+
+	//nop ??
+	public int[][] getCellules(){
+		return this.cellules;
+	}
+
+	public void updateGrille() {
+		int x = this.r.getPosX();
+		int y = this.r.getPosY();
+
+		// je suis en train de la faire	
+	}
+
+}
