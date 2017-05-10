ba5166af7ff7b87712d9914b6c818e24dd8bb525
Masson Brandon <brandonmasson@laposte.net>
2017-05-10 10:48:36 +0200

    Ajout de AfficheTablette.java et Serpentin.java

---
+++ AffichageTablette.java
@@ -0,0 +1,20 @@
+package robotTest;
+
+import java.awt.Graphics;
+import java.awt.GridLayout;
+
+import javax.swing.JPanel;
+
+public class AffichageTablette extends JPanel
+{
+	public AffichageTablette()
+	{
+		setLayout(new GridLayout(1, 4));
+		add(new Serpentin());
+	}
+	
+	public void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+	}
+}
