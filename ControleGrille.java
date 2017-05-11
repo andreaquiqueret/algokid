213f683ac029debf27e187ad9343b71e166dcb95
dietric22u <rider5.7@hotmail.fr>
2017-05-11 10:52:17 +0200

    Modification ControleGrille

--- ControleGrille.java
+++ ControleGrille.java
@@ -1,5 +1,4 @@
 import java.awt.Dimension;
-
 import javax.swing.JFrame;
 
 
@@ -11,11 +10,11 @@ public class ControleGrille {
 			JFrame fenetre = new JFrame("Controle Grille");
 		    fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		    AffichageGrille dessin = new AffichageGrille();
-		    dessin.setPreferredSize(new Dimension(700,700));
+		    dessin.setPreferredSize(new Dimension(800,800));
 		    fenetre.setContentPane(dessin);
 		    fenetre.pack();
 		    fenetre.setVisible(true);
-		    dessin.requestFocusInWindow();
-		    dessin.requestFocus();
+		    /*dessin.requestFocusInWindow();
+		    dessin.requestFocus();*/
 	  }
 }
