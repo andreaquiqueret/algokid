74f2ce48576209eba0ee1ad6d2307c1fa0facb7f
dietric22u <rider5.7@hotmail.fr>
2017-05-11 11:45:31 +0200

    Modification AffichageGrille

--- AffichageGrille.java
+++ AffichageGrille.java
@@ -7,25 +7,34 @@ public class AffichageGrille extends JPanel {
 	
 
 	public AffichageGrille () {
-		final JPanel grillage= new JPanel() {
-			public void paintComponent(Graphics g) {
-				for (int i=0; i < Grille.cellules.length; i++) {
 			
-					for (int j=0; j < Grille.cellules.length; j++) {
-						
-						if (Grille.cellules[i][j] == 0)
-							g.setColor(Color.green);
-						else 
-							g.setColor(Color.white);
-						g.fillRect(i+50, j+50, 50, 50);
-						g.setColor(Color.blue);
-						g.drawRect(i+50, j+50, 50, 50);
-					}
-				}
-			}
-		};
-	grillage.setSize (600, 600);
+		
+		/*grillage.setSize (600, 600);*/
+	
+	
+		/*this.setLayout(new BorderLayout());*/
+		/*this.add(grillage,BorderLayout.CENTER);*/
+	
 	}
 	
-	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g); 
+		for (int i=0; i < Grille.cellules.length; i++) {
+			
+			for (int j=0; j < Grille.cellules.length; j++) {
+				
+				if (Grille.cellules[i][j] == 0)
+					g.setColor(Color.green);
+				else 
+					g.setColor(Color.white);
+				g.fillRect(i+50, j+50, 50, 50);
+				g.setColor(Color.blue);
+				g.drawRect(i+50, j+50, 50, 50);
+			}
+		}
+	}
+	/*public void dessine() {
+		repaint();
+	}
+	*/
 }
