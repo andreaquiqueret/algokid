ed89e2ae7077fdc0a29ab7fd274a0928141e8ffb
Andrea Quiqueret <quiquere1u@univ-lorraine.fr>
2017-05-15 10:50:51 +0200

    config de la classe principal avec ajout des deux jpanle à l'interieur

--- Principal.java
+++ Principal.java
@@ -1,17 +1,26 @@
 import java.awt.Dimension;
 import java.io.IOException;
-
+import java.awt.*;
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.*;
 import javax.swing.JFrame;
 
-public class Principal 
+public class Principal extends JPanel
 {
 	public static void main(String[] args) throws IOException 
 	{
+		AffichageTablette panneau = new AffichageTablette();
+		AffichageGrille grille = new AffichageGrille();
+		JSplitPane contenu = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,true);
+		contenu.setDividerLocation(450);
+		contenu.setDividerSize(0);
+		contenu.add(panneau);
+		contenu.add(grille);
 		JFrame fenetre = new JFrame("AlgoKids");
 		fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		AffichageTablette panneau = new AffichageTablette();
-		fenetre.setPreferredSize(new Dimension(450, 480));
-		fenetre.setContentPane(panneau);
+		fenetre.setPreferredSize(new Dimension(1080, 650));
+		fenetre.getContentPane().add(contenu);
 		fenetre.pack();
 		
 		fenetre.setVisible(true);
