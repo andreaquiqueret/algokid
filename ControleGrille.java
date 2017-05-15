6022dce1c21139efca100eebdc3637beae1af6a2
dietric22u <rider5.7@hotmail.fr>
2017-05-15 10:15:36 +0200

    Modification taille JFrame ControleGrille

--- ControleGrille.java
+++ ControleGrille.java
@@ -10,7 +10,7 @@ public class ControleGrille {
 			JFrame fenetre = new JFrame("Controle Grille");
 		    fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		    AffichageGrille dessin = new AffichageGrille();
-		    dessin.setPreferredSize(new Dimension(800,800));
+		    dessin.setPreferredSize(new Dimension(500,480));
 		    fenetre.setContentPane(dessin);
 		    fenetre.pack();
 		    fenetre.setVisible(true);
